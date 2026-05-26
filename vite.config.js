5bccb40b91a5e9ed1a7026fb4a1af09a969b9631
Gaston Cordeiro <43023297+GastonCordeiro@users.noreply.github.com>
2026-05-26 11:15:20 -0300

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/comercios-app/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/dona-carmen/" : "/",
   plugins: [
     react(),
     tailwindcss(),
